c5e961758ef341b99d763842cc32f074c7a62993
Madhura Kulkarni <madhurakulkarni@Madhuras-MacBook-Pro.local>
2018-04-28 02:21:44 -0400

    First Sourcecode commit

---
+++ src/main/java/com/java/tutorial/collection/LinkedListExample.java
@@ -0,0 +1,15 @@
+package com.java.tutorial.collection;
+
+/**
+ * 
+ * @author madhurakulkarni
+ * 
+ * This class demostrates different use cases for Linkedlist
+ *
+ */
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World!");	
+	}
+}
